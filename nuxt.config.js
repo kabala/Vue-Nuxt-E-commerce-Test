05f7f9bb64e926b8a228c109d429f4a734313f5a
Carlos Santín <kabaldesign@gmail.com>
2021-11-29 03:27:01 -0500

    switched to SSR mode

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,8 +3,8 @@ import fs from 'fs'
 
 export default defineNuxtConfig({
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
-  // ssr: false,
-  target: 'static',
+  ssr: true,
+  target: 'server',
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
